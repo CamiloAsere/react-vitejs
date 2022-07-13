251d30b4376011ed8788535fb2ddbe45de522226
Camilo Alejandro Martinez Pujol <87444373+CamiloAsere@users.noreply.github.com>
2022-07-12 22:35:08 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ tele.ready()
 const onCheckout = () => {
     tele.MainButton.text = "Pay :)";
     tele.MainButton.show();
+    alert("troleado")
   };
   return (
     <div className="container" >
@@ -26,8 +27,8 @@ const onCheckout = () => {
         <b>
         <p>Hello Vite + React!</p>
         </b>
-        <button type="button" onClick={onCheckout}>
-            count is: {count}
+        <button type="button" className="btn btn-danger" onClick={onCheckout}>
+            mostrar botton extra
           </button>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
