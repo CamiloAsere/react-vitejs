065905405e7366883f118189ed8fb4fa98c71426
Camilo Alejandro Martinez Pujol <87444373+CamiloAsere@users.noreply.github.com>
2022-07-13 00:53:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState , useEffect } from 'react'
 import  background from "./assets/connected-city.jpg";
 import { Rutas } from './rutas';
 //Telegram.WebApp.offEvent('backButtonClicked', callback
-const tele=Telegram.WebApp.offEvent('backButtonClicked', callback)
+const tele=window.Telegram.WebApp.offEvent('backButtonClicked', callback)
 function App() {
   const [count, setCount] = useState(0)
   
