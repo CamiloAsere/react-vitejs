ea8b92d63c6cc531e26e7997b25acb4560df605b
CamiloAsere <camilo9mz5@gmail.com>
2022-07-20 23:58:37 -0700

    telegram

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
@@ -6,15 +5,15 @@ import Cart from "./Components/Cart/Cart";
 import { getData } from "./db/db";
 const foods = getData();
 
-//const tele = window.Telegram.WebApp;
+const tele = window.Telegram.WebApp;
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
-/*
+
   useEffect(() => {
     tele.ready();
   });
-*/
+
   const onAdd = (food) => {
     const exist = cartItems.find((x) => x.id === food.id);
     if (exist) {
@@ -59,50 +58,6 @@ function App() {
       </div>
     </>
   );
-=======
-import { useState , useEffect } from 'react'
-import  background from "./assets/connected-city.jpg";
-import { Rutas } from './rutas';
-//Telegram.WebApp.offEvent('backButtonClicked', callback
-const tele=window.Telegram.WebApp
-function App() {
-  const [count, setCount] = useState(0)
-  
-  const style = {
-    backgroundImage: `url(${background})`, 
-    width: "100%",
-    height: "400px",
-    
-  };
-useEffect( ( ) => {
-tele.ready()
-} )
-  
-const onCheckout = () => {
-    tele.MainButton.text = "Pay :)";
-    tele.MainButton.show();
-    alert("troleado")
-  };
-  return (
-    <div className="container" >
-      <header className="jumbotron" /*style={style}*/ >
-        <b>
-        <p>Hello Vite + React!</p>
-        </b>
-        <button type="button" className="btn btn-danger" onClick={onCheckout}>
-            mostrar botton extra
-          </button>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            nada
-          </button>
-        </p>
-       
-      </header>
-      <Rutas/>
-    </div>
-  )
->>>>>>> 8899b832a3473b13ebe0c378ff4fd7be1190bf67
 }
 
 export default App;
